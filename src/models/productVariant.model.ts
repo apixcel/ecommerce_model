3ee83c8b1dd42817d43c1a33be67248ed0af599f
Arko <arkoroyb@gmail.com>
2024-09-10 17:52:27 +0600

    models added

---
+++ src/models/productVariant.model.ts
@@ -0,0 +1,33 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface IProductVariant extends Document {
+  product_id: mongoose.Schema.Types.ObjectId;
+  color?: string;
+  size?: string;
+  price: number;
+  stock_quantity: number;
+}
+
+const productVariantSchema: Schema<IProductVariant> = new Schema(
+  {
+    product_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    color: { type: String },
+    size: { type: String },
+    price: { type: Number, required: true },
+    stock_quantity: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ProductVariantModel = mongoose.model<IProductVariant>(
+  "ProductVariant",
+  productVariantSchema
+);
+
+export default ProductVariantModel;
